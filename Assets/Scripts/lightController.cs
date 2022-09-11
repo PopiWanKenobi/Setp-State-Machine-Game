64fb6b1b09911aa01f5f72e86a60247710230a84
PopiWanKenobi <dylanpope@u.boisestate.edu>
2022-09-11 14:07:56 -0600

    Lanterns and Glowsticks
    Lanterns can now be turned on and off with the primary button while they're being held. Glowsticks turn on when being held and hovering with other controller

--- Assets/Scripts/lightController.cs
+++ Assets/Scripts/lightController.cs
@@ -6,22 +6,32 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 
 
-public class lightController : MonoBehaviour
+public class lightController : XRGrabInteractable
 {
     bool active;
     public bool beingHeld;
-    public InputActionReference lightActionReference;
+    public InputActionReference lightActionReference1;
+    public InputActionReference lightActionReference2;
 
 
-    void Holdable(SelectEnterEventArgs args)
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
-        beingHeld = !beingHeld;
+        beingHeld = true;
+        base.OnSelectEntered(args);
     }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        beingHeld = false;
+        base.OnSelectExited(args);
+    }
+
     // Update is called once per frame
     private void Update()
     {
-        lightActionReference.action.performed += LightSwitcher;
-        if(active == true)
+        lightActionReference1.action.performed += LightSwitcher;
+        lightActionReference2.action.performed += LightSwitcher;
+        if (active == true)
         {
             Debug.Log("Position to be sent to enemy:" + transform.position);
         }
@@ -29,10 +39,12 @@ public class lightController : MonoBehaviour
 
     void LightSwitcher(InputAction.CallbackContext obj)
     {
-
+        if (beingHeld == true)
+        {
             active = !active;
             GetComponent<Light>().enabled = active;
-            Debug.Log("button got pressed");
+        }
+
         
     }
 }
