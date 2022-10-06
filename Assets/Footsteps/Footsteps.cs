2615086627ca6ba150d33d4de548ef0b57f468cd
Kaden Baratcart <kadenbaratcart@u.boisestate.edu>
2022-10-05 18:19:31 -0600

    Audio

---
+++ Assets/Footsteps/Footsteps.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Footsteps : MonoBehaviour {
+
+	Camera mc;
+	
+	void Start ()	
+ 	{
+		mc = GetComponent<Camera>();
+	}
+	
+	void Update ()	
+ 	{
+		if(mc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
+		{
+			GetComponent<AudioSource>().volume = Random.Range(0.05f, 0.2f);
+			GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
+			GetComponent<AudioSource>().Play();
+		}
+	}
+}
